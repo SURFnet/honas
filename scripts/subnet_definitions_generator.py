47f2c253d51362c0fa4d15c56e51ef079f7907d9
Gijs Rijnders <gijs.rijnders@surfnet.nl>
2018-05-31 09:31:02 +0200

    Finished the subnet definitions generation script.

--- scripts/subnet_definitions_generator.py
+++ scripts/subnet_definitions_generator.py
@@ -17,7 +17,9 @@ import json
 parser = argparse.ArgumentParser(description='Honas subnet definitions file generator')
 parser.add_argument('-r', action='store', dest='input_file', help='Input file containing entity-prefix mappings', required=True)
 parser.add_argument('-w', action='store', dest='output_file', help='Output file receiving the JSON mappings')
+parser.add_argument('-v', action='store_true', dest='verbose', help='Verbose output')
 results = parser.parse_args()
+outfilename = results.output_file
 
 # Storing the mappings for generation.
 input_entities = {}
@@ -26,13 +28,13 @@ prefix_count = 0
 
 # Open the input file and read in the mappings.
 try:
-	with open(results.input_file, 'r') as infile:
-		reader = csv.reader(infile, delimiter=",")
+	with open(results.input_file, 'r', encoding='UTF-8', newline='') as infile:
+		reader = csv.DictReader(infile)
 
 		# Read out all rows from the input mapping file.
 		for row in reader:
-			prefix = ipaddress.ip_network(row[2])
-			entity = row[4]
+			prefix = row['Prefix']
+			entity = row['Volledige naam klant']
 			input_entities[entity] = 0
 			input_mappings[prefix] = entity
 			prefix_count += 1
@@ -42,12 +44,32 @@ except FileNotFoundError:
 	exit()
 
 # Some statistics about the input.
-print("Read " + str(len(input_mappings)) + " prefix-entity mappings from " + results.input_file)
-print("The input file contains " + str(prefix_count) + " prefixes and " + str(len(input_entities)) + " entities")
+if results.verbose:
+	print("Read " + str(len(input_mappings)) + " prefix-entity mappings from " + results.input_file)
+	print("The input file contains " + str(prefix_count) + " prefixes and " + str(len(input_entities)) + " entities")
+
+# Create basic JSON structure for the subnet activity data.
+json_data = { "subnet_activity" : [] }
+
+# Prepare the data in the correct format.
+for e, v in input_entities.items():
+	# Find all prefixes associated with the prefix.
+	associated_prefixes = []
+	for p, pe in input_mappings.items():
+		if e == pe:
+			ipaddr = ipaddress.ip_network(p)
+			associated_prefixes.append({ str(ipaddr.network_address) : ipaddr.prefixlen })
+
+	# Add entity to the JSON data.
+	json_data["subnet_activity"].append({ "entity" : e, "prefixes" : associated_prefixes })
 
 # Generate JSON from input.
-print(json.dumps(input_mappings))
-
-# Open the output file for JSON generation.
-#with open(results.output_file, 'w') as outfile:
+json_output = json.dumps(json_data)
+if outfilename is None:
+	print(json_output)
+else:
+	# Open the output file for JSON generation.
+	with open(outfilename, 'w') as outfile:
+		print(json_output, file=outfile)
+		print("Wrote JSON output to " + outfilename)
 
